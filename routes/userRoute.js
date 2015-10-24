e39f16613e65ad66871de5d6857d46bcb6e7c118
panda707 <agoswam3@ucsc.edu>
2015-10-24 01:02:10 -0700

    Add express-session, session token

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,6 +4,19 @@
 var express = require('express');
 var router = express.Router();
 
+// User presses link in email
+
+router.get('/:key?', function (req, res, next) {
+	if(req.params.key){
+	    var session = req.session;
+	    session.token = req.params.key;
+		res.redirect('/')
+	}
+	else{
+		next();
+	}
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
